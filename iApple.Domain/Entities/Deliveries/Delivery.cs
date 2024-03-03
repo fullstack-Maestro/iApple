c444be212d14bf719a979020c4b76aa07cbfec32
NematovaH <husnidabonu224@gmail.com>
2024-03-03 07:15:32 +0500

    Added Delivery models

---
+++ iApple.Domain/Entities/Deliveries/Delivery.cs
@@ -0,0 +1,12 @@
+﻿using iApple.Domain.Commons;
+
+namespace iApple.Domain.Entities.Deliveries;
+
+public class Delivery : Auditable
+{
+    public string Status { get; set; }
+    public string Address { get; set; }
+    public DateTime Date {  get; set; }
+    public long PurchaseId { get; set; }
+    public decimal DeliveryCost { get; set; }
+}
---
+++ iApple.Service/DTOs/Deliveries/DeliveryUpdateModel.cs
@@ -0,0 +1,10 @@
+﻿namespace iApple.Service.DTOs.Deliveries;
+
+public class DeliveryUpdateModel
+{
+    public DateTime Date { get; set; }
+    public string Status { get; set; }
+    public string Address { get; set; }
+    public long PurchaseId { get; set; }
+    public decimal DeliveryCost { get; set; }
+}

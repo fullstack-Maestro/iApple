283f2da962c03163fe74cffeb1cff34459451c7f
NematovaH <husnidabonu224@gmail.com>
2024-03-03 07:23:44 +0500

    Added interfaces

---
+++ iApple.Service/Interfaces/IDebitcardService.cs
@@ -0,0 +1,15 @@
+﻿using iApple.Service.DTOs.DebitCard;
+
+namespace iApple.Service.Interfaces;
+
+public interface IDebitcardService
+{
+    ValueTask<bool> DeleteAsync(int id);
+    ValueTask<bool> IsCardExpiredAsync(int id);
+    ValueTask<DebitCardViewModel> GetByIdAsync(int id);
+    ValueTask<List<DebitCardViewModel>> GetAllCardsAsync();
+    ValueTask<DebitCardViewModel> UpgradeBalanceAsync(int id, decimal amount);
+    ValueTask<DebitCardViewModel> DegradeBalanceAsync(int id, decimal amount);
+    ValueTask<DebitCardViewModel> CreateAsync(DebitCardCreationModel cardCreation);
+    ValueTask<DebitCardViewModel> UpdateAsync(int id, DebitCardUpdateModel cardUpdate);
+}
